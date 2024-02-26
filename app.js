376c0b6dd7c7f5086ef2b16401a0f247b91e1aeb
jheberer <jrheberer@gmail.com>
2024-02-26 11:09:28 -0500

    added error handling

--- app.js
+++ app.js
@@ -24,26 +24,44 @@ app.get('/', (req, res) => {
 app.get('/word/:word', async (req, res) => {
     const word = req.params.word;
 
-    const axios_response = await axios.get(`https://en.wiktionary.org/api/rest_v1/page/definition/${word}`);
+    try {
+      const axios_response = await axios.get(`https://en.wiktionary.org/api/rest_v1/page/definition/${word}`);
 
-    // res.send(axios_html)
-    const response_data = axios_response.data
-    const part_of_speech = response_data.en[0].partOfSpeech
-    const language = response_data.en[0].language
+      // res.send(axios_html)
+      const response_data = axios_response.data
+      const part_of_speech = response_data.en[0].partOfSpeech
+      const language = response_data.en[0].language
 
-    // parse the definition--an HTML expression--with cheerio
-    const $ = cheerio.load(response_data.en[0].definitions[0].definition);
-    const definition = $.text();
+      // parse the definition--an HTML expression--with cheerio
+      const $ = cheerio.load(response_data.en[0].definitions[0].definition);
+      const definition = $.text();
 
-    const word_package = {
-      'word': word,
-      'language of origin': language,
-      'part of speech': part_of_speech,
-      'common definition': definition
+      const word_package = {
+        'word': word,
+        'language of origin': language,
+        'part of speech': part_of_speech,
+        'common definition': definition
+      }
+      
+      // res.send(response_data.en[0].definitions[0].definition)
+      res.status(200).send(word_package)
+  } 
+  catch (error) {
+    if (error.response) {
+      const error_status = error.response.status;
+      // 404 returns when there's no matching wiktionary page
+      if (error_status === 404) {
+        res.status(404).send("Error 404: Word definition not found. Please confirm this is a valid English word.")
+      }
+      else {
+        res.status(error_status).send(`Error: ${error_status}. Please try again.`);
+      }
     }
+    else {
+      res.status(500).send('UNKNOWN ERROR');
+    }
+  }
     
-    // res.send(response_data.en[0].definitions[0].definition)
-    res.send(word_package)
 
     
 });
